1bc00089a4c9ec07705080e7b88801818b813727
Phillip Merriman <phillipmerriman@gmail.com>
2021-02-10 09:40:05 -0600

    added comments to html-routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,15 +1,19 @@
 const path = require('path');
 const express = require("express");
 
+// export all routes in one function
 module.exports = function (app) {
+    // Get/render homepage
     app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
+    // Get/render exercise page
     app.get("/exercise", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
 
+    // Get/render stats page
     app.get("/stats", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/stats.html"));
     });
